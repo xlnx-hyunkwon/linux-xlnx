0b2ac795b57243b07cf2a0f4169d900e673249c4
Hyun Kwon <hyun.kwon@xilinx.com>
2020-02-21 13:44:18 -0800

    [wip] media: i2c: max96705: default i2c addr
    Signed-off-by: Hyun Kwon <hyun.kwon@xilinx.com>

--- drivers/media/i2c/max96705.c
+++ drivers/media/i2c/max96705.c
@@ -191,6 +191,8 @@ static int max96705_initialize(struct max96705_device *dev)
 		return ret;
 	}
 
+	dev->max96705->addr = MAX96705_I2C_ADDRESS;
+
 	/* FIXME: why is SEREN needed here? */
 	ret = max96705_write(dev, MAX96705_MAIN_CONTROL,
 			     MAX96705_MAIN_CONTROL_SEREN |
